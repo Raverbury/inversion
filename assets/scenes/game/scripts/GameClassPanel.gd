beb4aa33601ed59f1b4055ef84b6f931b2a142f5
Raverbury <smurfjp198@gmail.com>
2023-10-06 17:43:42 +0700

    class select ui

---
+++ assets/scenes/game/scripts/GameClassPanel.gd
@@ -0,0 +1,42 @@
+extends Panel
+
+@export var game_class_id: int
+var is_selected: bool = false
+var is_mouse_over: bool = false
+
+func _ready():
+	mouse_entered.connect(on_mouse_entered)
+	mouse_exited.connect(on_mouse_exited)
+	gui_input.connect(on_gui_input)
+	EventBus.game_class_selected.connect(on_game_class_selected)
+
+func on_mouse_entered():
+	is_mouse_over = true
+	auto_set_color()
+
+func on_mouse_exited():
+	is_mouse_over = false
+	auto_set_color()
+
+func on_gui_input(event: InputEvent):
+	if is_mouse_over:
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				EventBus.game_class_selected.emit(game_class_id)
+
+func on_game_class_selected(gcid):
+	if game_class_id == gcid:
+		is_selected = true
+	else:
+		is_selected = false
+	auto_set_color()
+
+func auto_set_color():
+	if is_mouse_over && is_selected:
+		modulate = Color.GREEN
+	elif is_mouse_over:
+		modulate = Color.TEAL
+	elif is_selected:
+		modulate = Color.LAWN_GREEN
+	else:
+		modulate = Color.WHITE
---
+++ assets/scenes/game/scripts/GameClassSelectControl.gd
@@ -0,0 +1,14 @@
+extends Panel
+
+var last_gc_id: int = -1
+@onready var confirm_button = $ConfirmButton
+
+func _ready():
+	EventBus.game_class_selected.connect(on_game_class_selected)
+	confirm_button.pressed.connect(on_confirm_button_pressed)
+
+func on_game_class_selected(gcid):
+	last_gc_id = gcid
+
+func on_confirm_button_pressed():
+	pass
