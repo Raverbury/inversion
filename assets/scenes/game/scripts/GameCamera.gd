3eccf0a0b6a51ce5d6713963dd204450b8c61177
Raverbury <smurfjp198@gmail.com>
2023-11-28 22:54:17 +0700

    dynamic camera boundaries

--- assets/scenes/game/scripts/GameCamera.gd
+++ assets/scenes/game/scripts/GameCamera.gd
@@ -4,6 +4,8 @@ var max_camera_zoom: Vector2 = Vector2(4.0, 4.0)
 var min_camera_zoom: Vector2 = Vector2(0.8, 0.8)
 var max_x: float = 800
 var max_y: float = 500
+var min_x: float = -800
+var min_y: float = -500
 var pan_sensitivity: float = 10.0
 
 var is_in_tween: bool = false
@@ -16,6 +18,7 @@ func _ready():
 	EventBus.camera_force_panned.connect(__camera_force_panned_handler)
 	EventBus.camera_panned.connect(__camera_panned_handler)
 	EventBus.camera_zoomed.connect(__camera_zoomed_handler)
+	EventBus.camera_bounds_updated.connect(__camera_bounds_updated_handler)
 
 
 func __camera_force_panned_handler(pos: Vector2, duration: float):
@@ -32,11 +35,18 @@ func __force_panned_tween_finished():
 func __camera_panned_handler(pan_direction: Vector2):
 	if is_in_tween == true:
 		return
-	var to_be_x = clampf(position.x + pan_direction.x * pan_sensitivity/zoom.x, -max_x, max_x)
-	var to_be_y = clampf(position.y + pan_direction.y * pan_sensitivity/zoom.y, -max_y, max_y)
+	var to_be_x = clampf(position.x + pan_direction.x * pan_sensitivity/zoom.x, min_x, max_x)
+	var to_be_y = clampf(position.y + pan_direction.y * pan_sensitivity/zoom.y, min_y, max_y)
 	global_position = Vector2(to_be_x, to_be_y)
 
 
 func __camera_zoomed_handler(zoom_direction: int):
 	var to_be_zoom = zoom + (zoom_direction * Vector2(0.08, 0.08) * zoom)
 	zoom = clamp(to_be_zoom, min_camera_zoom, max_camera_zoom)
+
+
+func __camera_bounds_updated_handler(max_x_mg, min_x_mg, max_y_mg, min_y_mg):
+	max_x = Global.Util.global_pos_at(max_x_mg)
+	min_x = Global.Util.global_pos_at(min_x_mg)
+	max_y = Global.Util.global_pos_at(max_y_mg)
+	min_y = Global.Util.global_pos_at(min_y_mg)
--- assets/scenes/game/scripts/GameTileMap.gd
+++ assets/scenes/game/scripts/GameTileMap.gd
@@ -2,6 +2,11 @@ class_name GameTileMap extends TileMap
 
 @export var map_name: String
 @export var spawn_points: Array[Vector2i]
+@export var max_x_mapgrid: int
+@export var min_x_mapgrid: int
+@export var max_y_mapgrid: int
+@export var min_y_mapgrid: int
+
 var selected_tile: Vector2
 var has_last_selected: bool = false
 var reachables: Array = []
@@ -10,15 +15,37 @@ var movement_path: Array = []
 var attack_targets: Array = []
 var not_enough_ap: bool
 
-enum TILE_LAYER {BACKGROUND, HIGHLIGHT, SELECT, HIGHLIGHT2}
 const HIGHLIGHT_GREEN = Vector2i(0, 0)
 const HIGHLIGHT_RED = Vector2i(1, 0)
 const HIGHLIGHT_BLUE = Vector2i(0, 1)
 const HIGHLIGHT_YELLOW = Vector2i(1, 1)
 
+const HIGHLIGHT_SELECT_GREEN = Vector2i(0, 0)
+const HIGHLIGHT_SELECT_RED = Vector2i(1, 0)
+const HIGHLIGHT_SELECT_LIGHTBLUE = Vector2i(0, 1)
+const HIGHLIGHT_SELECT_LIGHTYELLOW = Vector2i(1, 1)
+const HIGHLIGHT_SELECT_PURPLE = Vector2i(0, 2)
+const HIGHLIGHT_SELECT_ORANGE = Vector2i(1, 2)
+const HIGHLIGHT_SELECT_WHITE = Vector2i(0, 3)
+const HIGHLIGHT_SELECT_BLACK = Vector2i(1, 3)
+
+const LAYER_SURFACE = 0
+const LAYER_OBSTACLE = 1
+const LAYER_DECORATION = 2
+const LAYER_SELECT = 3
+const LAYER_HIGHLIGHT = 4
+const LAYER_HIGHLIGHT_SELECT = 5
+
+const SOURCE_TILESET = 0
+const SOURCE_SELECT = 1
+const SOURCE_HIGHLIGHT = 2
+const SOURCE_HIGHLIGHT_SELECT = 3
+
 
 func get_data_at(mapgrid: Vector2, data_name: String, fallback_value):
-	var tile_data: TileData = get_cell_tile_data(0, mapgrid)
+	var tile_data: TileData = get_cell_tile_data(LAYER_OBSTACLE, mapgrid)
+	if tile_data == null:
+		tile_data = get_cell_tile_data(LAYER_SURFACE, mapgrid)
 	return tile_data.get_custom_data(data_name) if not tile_data == null else fallback_value
 
 
@@ -33,14 +60,22 @@ func get_stat_mods_at(mapgrid: Vector2):
 		"armor_mod": get_data_at(mapgrid, "armor_mod", 0)
 	}
 
+
+func get_atlas_coord_at(mapgrid: Vector2):
+	var tile_data: TileData = get_cell_tile_data(LAYER_OBSTACLE, mapgrid)
+	if tile_data == null:
+		return get_cell_atlas_coords(LAYER_SURFACE, mapgrid) as Vector2
+	return get_cell_atlas_coords(LAYER_OBSTACLE, mapgrid) as Vector2
+
+
 func select_tile(tile_mapgrid: Vector2):
 	if has_last_selected == true:
 		if tile_mapgrid == selected_tile:
 			return
-		set_cell(2, selected_tile, -1, get_cell_atlas_coords(0, selected_tile))
+		set_cell(LAYER_SELECT, selected_tile, -1, get_cell_atlas_coords(0, selected_tile))
 	has_last_selected = true
 	selected_tile = tile_mapgrid
-	set_cell(2, selected_tile, 2, Vector2i(0, 0))
+	set_cell(LAYER_SELECT, selected_tile, SOURCE_SELECT, Vector2i(0, 0))
 
 
 func set_reachables(_reachables):
@@ -50,12 +85,12 @@ func set_reachables(_reachables):
 
 func show_reachables():
 	for reachable_tile in reachables:
-		set_cell(TILE_LAYER.HIGHLIGHT, reachable_tile, 1, HIGHLIGHT_GREEN)
+		set_cell(LAYER_HIGHLIGHT, reachable_tile, SOURCE_HIGHLIGHT, HIGHLIGHT_GREEN)
 
 
 func hide_reachables():
 	for reachable_tile in reachables:
-		set_cell(TILE_LAYER.HIGHLIGHT, reachable_tile, -1, Vector2i(-1, -1))
+		set_cell(LAYER_HIGHLIGHT, reachable_tile, -1, Vector2i(-1, -1))
 
 
 func set_movement_path(_movement_path, _not_enough_ap):
@@ -66,12 +101,12 @@ func set_movement_path(_movement_path, _not_enough_ap):
 
 func show_movement_path():
 	for tile in movement_path:
-		set_cell(TILE_LAYER.HIGHLIGHT2, tile, 1, HIGHLIGHT_RED if not_enough_ap else HIGHLIGHT_BLUE)
+		set_cell(LAYER_HIGHLIGHT_SELECT, tile, SOURCE_HIGHLIGHT_SELECT, HIGHLIGHT_SELECT_RED if not_enough_ap else HIGHLIGHT_SELECT_LIGHTBLUE)
 
 
 func hide_movement_path():
 	for tile in movement_path:
-		set_cell(TILE_LAYER.HIGHLIGHT2, tile, -1, Vector2i(-1, -1))
+		set_cell(LAYER_HIGHLIGHT_SELECT, tile, -1, Vector2i(-1, -1))
 
 
 func set_attackables(_attackables):
@@ -82,13 +117,13 @@ func set_attackables(_attackables):
 func show_attackables():
 	for attackable_tile in attackables.keys():
 		var ranged_acc_mod = attackables[attackable_tile]
-		set_cell(TILE_LAYER.HIGHLIGHT, attackable_tile, 1, HIGHLIGHT_RED if ranged_acc_mod < 1.0 else
+		set_cell(LAYER_HIGHLIGHT, attackable_tile, SOURCE_HIGHLIGHT, HIGHLIGHT_RED if ranged_acc_mod < 1.0 else
 			(HIGHLIGHT_YELLOW if ranged_acc_mod == 1.0 else HIGHLIGHT_GREEN))
 
 
 func hide_attackables():
 	for attackable_tile in attackables.keys():
-		set_cell(TILE_LAYER.HIGHLIGHT, attackable_tile, -1, Vector2i(-1, -1))
+		set_cell(LAYER_HIGHLIGHT, attackable_tile, -1, Vector2i(-1, -1))
 
 
 func set_attack_target(_attack_target, _not_enough_ap):
@@ -99,9 +134,9 @@ func set_attack_target(_attack_target, _not_enough_ap):
 
 func show_attack_target():
 	for attack_target in attack_targets:
-		set_cell(TILE_LAYER.HIGHLIGHT2, attack_target, 1, HIGHLIGHT_RED if not_enough_ap else HIGHLIGHT_GREEN)
+		set_cell(LAYER_HIGHLIGHT_SELECT, attack_target, SOURCE_HIGHLIGHT_SELECT, HIGHLIGHT_SELECT_RED if not_enough_ap else HIGHLIGHT_SELECT_ORANGE)
 
 
 func hide_attack_target():
 	for attack_target in attack_targets:
-		set_cell(TILE_LAYER.HIGHLIGHT2, attack_target, -1, Vector2i(-1, -1))
+		set_cell(LAYER_HIGHLIGHT_SELECT, attack_target, -1, Vector2i(-1, -1))
